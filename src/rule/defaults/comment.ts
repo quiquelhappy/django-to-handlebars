2b2a813ffe64a74870519f67d3d5c888e2f1676b
quiquelhappy <quiquelhappy@gmail.com>
2022-07-21 20:23:23 +0200

    comments

---
+++ src/rule/defaults/comment.ts
@@ -0,0 +1,22 @@
+import { rule } from "../rule";
+
+const SINGLE_LINE: rule = {
+    regex: /{#\s*(.*)\s*#}/g,
+    replace: new Map()
+        .set(/{#\s*/g, '{{! ')
+        .set(/\s*#}/g, '}}')
+}
+
+const MULTI_LINE_START: rule = {
+    regex: /{%\s*comment(\s{1,}".*")?\s*%}/g,
+    replace: new Map()
+        .set(/.*/, '{{!--')
+}
+
+const MULTI_LINE_END: rule = {
+    regex: /{%\s*endcomment\s*%}/g,
+    replace: new Map()
+        .set(/.*/, ' --}}')
+}
+
+export const COMMENT = [SINGLE_LINE, MULTI_LINE_START, MULTI_LINE_END]
