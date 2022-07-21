a594adf4515919d83eb0087e3defad687a5b923d
quiquelhappy <quiquelhappy@gmail.com>
2022-07-21 21:10:35 +0200

    else

--- src/rule/defaults/if.ts
+++ src/rule/defaults/if.ts
@@ -1,10 +1,16 @@
 import { rule } from "../rule";
 
 const IF_START: rule = {
-    regex: /{%\s*if\s{1,}([a-zA-Z0-9_]*(?:\.[\w-]+)*\s*)%}/g,
+    regex: /{%\s*if\s{1,}([a-zA-Z0-9_]*(?:\.[\w]+)*\s*)%}/g,
     replace: new Map()
-    .set(/{%\s*/g, '{{#')
-    .set(/\s*%}/g, '}}')
+        .set(/{%\s*/g, '{{#')
+        .set(/\s*%}/g, '}}')
+}
+
+const IF_ELSE: rule = {
+    regex: /{%\s*else\s*%}/g,
+    replace: new Map()
+        .set(/.*/, '{{else}}')
 }
 
 const IF_END: rule = {
@@ -13,4 +19,4 @@ const IF_END: rule = {
         .set(/.*/, '{{/if}}')
 }
 
-export const IF = [IF_START, IF_END]
+export const IF = [IF_START, IF_ELSE, IF_END]
