c137bddde28ab8f36412313d3e8f772c372129b4
quiquelhappy <quiquelhappy@gmail.com>
2022-07-21 20:23:37 +0200

    test

---
+++ test/convert.ts
@@ -0,0 +1,19 @@
+import Transpiler from "../src/Transpiler";
+
+const transpiler = new Transpiler()
+const test = (input: string) => {
+    console.log({
+        input: input,
+        result: transpiler.convert(input)
+    })
+}
+
+// variables
+test('My first name is {{ first_name }}. My last name is {{ last_name }}.')
+
+// basic conditionals
+test('{% if user.is_authenticated %}Hello, {{ user.username }}.{% endif %}')
+
+// comments
+test('{# this won\'t be rendered #}')
+test(`{% comment "Optional note" %} this won\'t be rendered {% endcomment %}`)
