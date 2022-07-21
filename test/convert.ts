e869e5305cd805bd39b854950257c6d37a6db1ff
quiquelhappy <quiquelhappy@gmail.com>
2022-07-21 21:10:55 +0200

    iterator test

--- test/convert.ts
+++ test/convert.ts
@@ -12,8 +12,11 @@ const test = (input: string) => {
 test('My first name is {{ first_name }}. My last name is {{ last_name }}.')
 
 // basic conditionals
-test('{% if user.is_authenticated %}Hello, {{ user.username }}.{% endif %}')
+test('{% if user.is_authenticated %}Hello, {{ user.username }}.{% else %} not authenticated {% endif %}')
 
 // comments
 test('{# this won\'t be rendered #}')
 test(`{% comment "Optional note" %} this won\'t be rendered {% endcomment %}`)
+
+// iterator
+test('{% for o in some_list %}{% endfor %}')
