844cfb3e1c5f26abe48ffb3737e4e4aa751fe5cf
quiquelhappy <quiquelhappy@gmail.com>
2022-07-22 11:08:00 +0200

    iterator object name carry

--- src/rule/defaults/iterator.ts
+++ src/rule/defaults/iterator.ts
@@ -1,11 +1,16 @@
-import { rule } from "../rule";
+import { rule, ruleGroup } from "../rule";
 
 // {% for o in some_list %}
 const FOR_START: rule = {
     regex: /{%\s*for\s{1,}[a-zA-Z0-9_]\s{1,}in\s{1,}([a-zA-Z0-9_]*(?:\.[\w]+)*\s*)%}/g,
+    variables: new Map()
+        .set('object', {
+            regex: /{%\s*for\s{1,}([a-zA-Z0-9_]*(?:\.[\w]+)*)*?\s{1,}in/,
+            group: 1
+        }),
     replace: new Map()
         .set(/{%\s*for\s{1,}[a-zA-Z0-9_]\s{1,}in\s{1,}/g, '{{#each ')
-        .set(/\s*%}/g, '}}')
+        .set(/\s*%}/g, ' as | $variable:object |}}'),
 }
 
 const FOR_END: rule = {
@@ -14,4 +19,6 @@ const FOR_END: rule = {
         .set(/.*/, '{{/each}}')
 }
 
-export const ITERATOR = [FOR_START, FOR_END]
+export const ITERATOR: ruleGroup = {
+    rules: [FOR_START, FOR_END]
+}
