ae5e2bb6100dcaaaad609a4b68a1b93b9a9e0346
quiquelhappy <quiquelhappy@gmail.com>
2022-07-22 11:06:19 +0200

    rule groups and variables

--- src/rule/rule.ts
+++ src/rule/rule.ts
@@ -1,11 +1,23 @@
 import { COMMENT } from "./defaults/comment"
 import { IF } from "./defaults/If"
+import { INCLUDE } from "./defaults/include"
 import { ITERATOR } from "./defaults/iterator"
 import { VARIABLE } from "./defaults/Variable"
 
-export type rule = {
+type variable = {
     regex: RegExp,
-    replace: Map<RegExp, string>
+    group: number,
+    default: string
 }
 
-export const DEFAULTS: rule[] = [...COMMENT, ...IF, ...ITERATOR, VARIABLE]
+export type rule = {
+    regex: RegExp,
+    replace: Map<RegExp, string>,
+    variables?: Map<string, variable>,
+}
+
+export type ruleGroup = {
+    rules: rule[],
+}
+
+export const DEFAULTS: ruleGroup[] = [COMMENT, IF, INCLUDE, ITERATOR, VARIABLE]
